bed314a9e469ba7a68a84493051d035640d67ce2
unknown <amit423raja@gmail.com>
2023-06-29 15:51:49 +0530

    added vpaid keys

--- app.js
+++ app.js
@@ -95,32 +95,32 @@ app.get('/health', (req, res, next) => {
   res.send("ok");
 })
 
-app.use(async (req, res, next) => {
-  try {
-    const accessToken = req.headers.authorization.split(" ")[1];
+// app.use(async (req, res, next) => {
+//   try {
+//     const accessToken = req.headers.authorization.split(" ")[1];
 
-    const { data } = await axios.post(
-      COGNITO_URL,
-      {
-        AccessToken: accessToken
-      },
-      {
-        headers: {
-          "Content-Type": "application/x-amz-json-1.1",
-          "X-Amz-Target": "AWSCognitoIdentityProviderService.GetUser"
-        }
-      }
-    )
+//     const { data } = await axios.post(
+//       COGNITO_URL,
+//       {
+//         AccessToken: accessToken
+//       },
+//       {
+//         headers: {
+//           "Content-Type": "application/x-amz-json-1.1",
+//           "X-Amz-Target": "AWSCognitoIdentityProviderService.GetUser"
+//         }
+//       }
+//     )
 
-    req.user = data.UserAttributes[2].Value
-    next();
-  } catch (error) {
-    console.log(error);
-    return res.status(401).json({
-      message: 'Auth failed'
-    });
-  }
-})
+//     req.user = data.UserAttributes[2].Value
+//     next();
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(401).json({
+//       message: 'Auth failed'
+//     });
+//   }
+// })
 
 seq.sync().then(result => {
   // console.log(result);
